ec0ef20f7afac97f13f630bc38e1a42df5de5eeb
sergioserg835 <sergioserg835@gmail.com>
2021-03-06 20:44:23 +0200

    feat: add code for three function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {
+    return 0;
+  }
+  else {
+    let myMin = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < myMin) myMin = array[i];
+    }
+    return myMin;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {
+    return 0;
+}
+else {
+    let myMax = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > myMax) myMax = array[i];
+    }
+    return myMax;
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0) {
+    return 0;
+  }
+  else {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    let myAvg = sum / array.length;
+    return myAvg;
+  }
 }
